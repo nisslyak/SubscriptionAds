14e2e6684174a07fda01a9ee027878d0de68d1df
nisslyak <nissopodolyak@gmail.com>
2024-09-27 11:04:15 +1000

    Add project files.

---
+++ SubscriptionAds/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SubscriptionAds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            List<User> users = new List<User>
+            {
+                new User { Login = "user1", Name = "Masha", IsPremium = false },
+                new User { Login = "user2", Name = "Pasha", IsPremium = true },
+                new User { Login = "user3", Name = "Kolya", IsPremium = false }
+            };
+            foreach (var user in users)
+            {
+                Console.WriteLine("Hello " + user.Name);
+                if (!user.IsPremium)
+                    Ads.ShowAds();
+            }
+        }
+    }
+}
